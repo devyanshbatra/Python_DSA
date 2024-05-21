74de979a64b58b413d04c39a298deff058980f62
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-05-21 14:44:25 +0530

    Add files via upload

---
+++ 2D_array.py
@@ -0,0 +1,6 @@
+import numpy as np
+import array
+Two_Array=np.array([[1,2,3,4,],[7,8,9,1]])
+print(Two_Array)
+# time complexity of 2d array is Omn 
+
---
+++ Column_Insertion.py
@@ -0,0 +1,6 @@
+import numpy as np
+import array
+Two_Array=np.array([[1,2,3,4,],[7,8,9,1]])
+newTwo_array=np.insert(Two_Array,0,[[1,2,3,4]],axis=0)
+print(newTwo_array)
+# INSERTION OF THE ROW IN THE ABOVE LINES OF CODE WITH THE HELP OF THE AXIS AS SET AS 0

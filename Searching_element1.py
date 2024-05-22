9c47afc5a44bc19d89e07df3c4d0fff3614982e9
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-05-22 12:07:51 +0530

    Add files via upload

---
+++ Searching_element1.py
@@ -0,0 +1,13 @@
+# Searching an element in the list in python
+my_list=[10,20,30,40,50,60,70]
+print(my_list)
+target = 50
+if target in my_list:
+ print(f"{target} is in the list")
+else :
+ print(f"{target}is not in the list")
+def linearSearch(p_list,p_target)
+ for i,value in enumerate(p_list):
+  
+ 
+         

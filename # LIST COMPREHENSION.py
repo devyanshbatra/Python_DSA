ce2ec67cc72fd24b399d7152f75d61a00be288a7
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-05-27 15:59:05 +0530

    Add files via upload

---
+++ # LIST COMPREHENSION.py
@@ -0,0 +1,14 @@
+# LIST COMPREHENSION
+prev_list=[1,2,3,4]
+new_list=[]
+for i in prev_list:
+    multiply_2=i*2
+    new_list.append(multiply_2)
+print(new_list)
+
+prev_list1=[5,6,7,8,9]
+new_list1=[]
+for i in prev_list1:
+    multiply_2_1=i*2
+    new_list1.append(multiply_2_1)
+print(new_list1)

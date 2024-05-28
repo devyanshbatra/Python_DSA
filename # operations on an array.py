37a8087c23a3587d67c31fe589302362b6e3c848
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-05-28 12:57:23 +0530

    Add files via upload

---
+++ # operations on an array.py
@@ -0,0 +1,15 @@
+# operations on an array 
+import numpy as np
+def operational_array:
+sum=0
+product=0
+  for i in range(len(array)):
+  sum+=i
+  for i in range(len(array)):
+  product*=i
+print("sum="+"str(sum)+","product="+"str(product)")
+operational_array=np.array(1,2,3,4,5,6)
+operational_array()
+
+
+   

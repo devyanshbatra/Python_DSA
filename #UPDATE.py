5bf60b1003df56f8b9cca93f3f8a665caee31091
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-05-31 17:17:29 +0530

    Add files via upload

---
+++ #UPDATE.py
@@ -0,0 +1,6 @@
+# upddate element adds the element of the new dict to the orignal dict
+my_dict={"name":"devyansh","age":"20","address":"faridabad","education":"masters"}
+print(my_dict)
+new_dict={"a":1,"b":2,"c":3}
+my_dict.update(new_dict)
+print(my_dict)

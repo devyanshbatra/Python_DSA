ebfcec9cbd9f406d56920e634d5008d6bf12834c
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-05-31 17:14:37 +0530

    Add files via upload

---
+++ POP_ITEM.py
@@ -0,0 +1,4 @@
+# set default is used to edit the dictionary and add elementinto it
+my_dict={"name":"devyansh","age":"20","address":"faridabad","education":"masters"}
+print(my_dict.setdefault("name1","added"))
+print(my_dict)

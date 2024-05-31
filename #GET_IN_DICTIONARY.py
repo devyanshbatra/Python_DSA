ca567197c0d0b857abe22b19a297516bfb23303a
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-05-31 17:06:57 +0530

    Add files via upload

---
+++ #GET_IN_DICTIONARY.py
@@ -0,0 +1,3 @@
+# get is used to access a single elements from the dicitonary
+my_dict={"name":"devyansh","age":"20","address":"faridabad","education":"masters"}
+print(my_dict.get("age",27))
---
+++ DICTIONARY_OPERATIONS.py
@@ -0,0 +1,12 @@
+my_dict={"name":"devyansh","age":"20","address":"faridabad","education":"masters"}
+print(my_dict)
+# clear is used to remove all the elements from the dictionary
+my_dict.clear()
+print(my_dict)
+
+# the copy method is used to copy the dictionary
+dict=my_dict.copy()
+print(my_dict)
+print(dict)
+
+

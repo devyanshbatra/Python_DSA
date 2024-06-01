0ee1e4af8fb6c5293e76c137ac3a5d4634297e59
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-01 16:28:27 +0530

    Add files via upload

---
+++ # Tuple_Operations.py
@@ -0,0 +1,8 @@
+# Tuple_Operations
+new_tuple=(1,2,3,4,5)
+new_tuple1=(6,7,8,9,10)
+print(new_tuple * 4)
+print(new_tuple1 * 4)
+print(new_tuple.index(3))
+print("3" in new_tuple1)
+print(len(new_tuple))

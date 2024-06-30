79e260e14e7706038828731f154fc305bcc93804
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-30 20:27:06 +0530

    Add files via upload

---
+++ SETS.py
@@ -0,0 +1,11 @@
+# SETS 
+set={1,2,3,4,5,"Hello"}  # we can not add list in the sit and they are unordered collecitons of elements
+type(set)
+print(set)
+set.add(50) # doesntr add the element at the last position add it to the unordered position
+print(set)
+set.remove(50)
+print(set)
+set2={7,8,9}
+print(set.union(set2))
+

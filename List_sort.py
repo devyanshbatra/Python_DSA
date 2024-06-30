5487b2e8bf7671f2ad2856ec967114b4ffe218f5
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-06-30 16:05:32 +0530

    Add files via upload

---
+++ List_sort.py
@@ -0,0 +1,9 @@
+list_1=[12,12.3,"string","hi"]
+list_1.extend([12,23])
+print(list_1)
+list_1.insert(3,"mewww") # inserts the element at the desired element at the postion
+list_2=["hello","love","babes"]
+print(list_2)
+list_2.sort(reverse = True)
+print(list_2)
+
---
+++ Nested_List.py
@@ -0,0 +1,3 @@
+list_1=[12,12.3,"string","hi"]
+print(list_1)
+list_1.extend([12,23])

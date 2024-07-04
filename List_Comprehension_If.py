3be5e98f4cb3a29ccf373347cea8a5e24bfd4bcc
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-07-04 22:13:34 +0530

    Add files via upload

---
+++ List_Comprehension_If.py
@@ -0,0 +1,3 @@
+# List comprehension with the if statement and the for loop
+list=[i for i in range(13) if i%2==0]
+print(list)
---
+++ List_Comprehension_for.py
@@ -0,0 +1,7 @@
+list_natural=[i for i in range(10)]
+print(list_natural)
+
+list_output=[]
+for i in range(10):
+ list_output.append(i)
+print(list_output)

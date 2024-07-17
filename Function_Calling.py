b986ba759050253126b12ee5bcd118c0a429544b
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-07-17 17:47:56 +0530

    Add files via upload

---
+++ Function_Calling.py
@@ -0,0 +1,12 @@
+def sum(x,y):
+    print(x+y)
+def sub(x,y):
+    print(x-y)
+def mul(x,y):
+    print(x*y)
+def mas_cal(var1,var2):
+    a=sum(var1,var2)
+    b=sub(var1,var2)
+    c=mul(var1,var2)
+print(a,b,c)
+mas_cal(10,5)

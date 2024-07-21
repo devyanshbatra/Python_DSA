8061f84ce683c510f21193c9456db09bdaff6a7b
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2024-07-21 21:21:32 +0530

    Add files via upload

---
+++ #LAMBDA FUNCTIONS.py
@@ -0,0 +1,8 @@
+#LAMBDA FUNCTIONS
+x=lambda a:a*10
+x(10)
+y=lambda a,b:a+b
+y(10,20)
+var=lambda a,b:"Hey" if a%b==0 else "bye"
+list=[1,2,3,4,5,6,7,8,9]
+list(map(lambda a:"Even" if a%2==0 else"odd"))

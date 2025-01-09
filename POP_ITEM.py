b1082de4c3bbde39fa9b2cd176c486031820a234
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2025-01-09 11:10:38 +0530

    Add files via upload

--- POP_ITEM.py
+++ POP_ITEM.py
@@ -1,4 +1,37 @@
-# set default is used to edit the dictionary and add elementinto it
-my_dict={"name":"devyansh","age":"20","address":"faridabad","education":"masters"}
-print(my_dict.setdefault("name1","added"))
-print(my_dict)
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class Linked_List:
+    def __init__(self,value):
+        new_node=Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1 
+
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+         self.tail.next=new_node
+         self.tail=new_node
+
+    def pop(self):
+        if self.length==0:
+            return None
+    temp=self.head
+    pre=self.head
+
+    while(temp.next):
+            pre = temp
+            temp = temp.next
+    self.tail = pre
+    self.tail.next = None
+    self.length -= 1
+
+my_Linked_List=Linked_List(6) 
+my_Linked_List.append(7)
+print(my_Linked_List.pop(2))

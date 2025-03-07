192e7610d217584eea1115d4ecce364f4ea0d8d7
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2025-03-07 23:20:21 +0530

    Add files via upload

---
+++ RemoveHeap.py
@@ -0,0 +1,32 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap=[]
+
+    def left_node(self,index):
+        return 2*index +1
+    
+    def right_child(self, index):
+        return 2 * index + 2
+
+    def parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and self.heap[current] > self.heap[self.parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+    def remove(self):
+        if len(self.heap)==0:
+            return None
+        if len(self.heap)==1:
+            return self.heap.pop()
+        max_value=self.heap[0]
+        self.heap[0]=self.heap.pop()
+        self.sinkdown(0)
+        return max_value
